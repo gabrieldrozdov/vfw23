6cd2c8b5e25ec90d07f5fca2d7b63222b86fa362
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2023-01-28 14:43:56 -0500

    site built out except for instruments and syllabus

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// —————————————————————————————————————————————————————————————————————
+// INTRO AND LOGO
+// —————————————————————————————————————————————————————————————————————
+
 // Logo functions
 function logoIn() {
 	let logoLine1 = document.querySelector(".logo-line-1");
@@ -13,7 +17,7 @@ function logoIn() {
 }
 function logoOut() {
 	let logo = document.querySelector(".logo");
-	logo.style.transform = "translateX(100vw) rotate(10deg)";
+	logo.style.transform = "translateY(-100vh) rotate(10deg)";
 	logoAnimationLoop = false;
 }
 let logoAnimationLoop = true;
@@ -80,8 +84,36 @@ function colorCycle() {
 		}
 	}, 5000);
 }
+// Intro on page load
+setTimeout(() => {
+	let logoLine1 = document.querySelector(".logo-line-1");
+	let logoLine2 = document.querySelector(".logo-line-2");
+	let logoLine3 = document.querySelector(".logo-line-3");
+	logoLine1.style.transition = "0s";
+	logoLine2.style.transition = "0s";
+	logoLine3.style.transition = "0s";
+	logoLine1.style.transform = `translateY(-75vh) rotate(${Math.random()*90-45}deg)`;
+	logoLine2.style.transform = `translateX(-75vw) rotate(${Math.random()*90-45}deg)`;
+	logoLine3.style.transform = `translateY(75vh) rotate(${Math.random()*90-45}deg)`;
+	setTimeout(() => {
+		logoLine1.style.transition = "transform 2s, background-color .5s, color .5s, fill .5s";
+		logoLine2.style.transition = "transform 2s, background-color .5s, color .5s, fill .5s";
+		logoLine3.style.transition = "transform 2s, background-color .5s, color .5s, fill .5s";
+		logoIn();
+	}, 50);
+	setTimeout(() => {
+		logoAnimation();
+		colorCycle();
+	}, 2000);
+}, 50);
 
-// Navigation and settings functions
+
+
+// —————————————————————————————————————————————————————————————————————
+// NAVIGATION AND SETTINGS
+// —————————————————————————————————————————————————————————————————————
+
+// Nav and settings sidebar
 function navIn() {
 	let nav = document.querySelector("#nav");
 	nav.style.transform = "translateX(0) rotate(0deg)";
@@ -121,6 +153,7 @@ function settingsOut() {
 		navIn();
 	}, 250);
 }
+// Settings functions
 function settingsFontSize(value) {
 	let currentValue = getComputedStyle(document.documentElement).getPropertyValue('--player-fontsize');
 	document.querySelector(':root').style.setProperty("--player-fontsize", parseFloat(currentValue) + value + "vw");
@@ -156,35 +189,10 @@ function settingsChangeColor() {
 	body.style.backgroundImage = `url("graphics/background-${color}.gif")`;
 	body.style.backgroundSize = `${Math.random()*50+50}px ${Math.random()*50+50}px`;
 }
-
-// Intro on page load
-setTimeout(() => {
-	let logoLine1 = document.querySelector(".logo-line-1");
-	let logoLine2 = document.querySelector(".logo-line-2");
-	let logoLine3 = document.querySelector(".logo-line-3");
-	logoLine1.style.transition = "0s";
-	logoLine2.style.transition = "0s";
-	logoLine3.style.transition = "0s";
-	logoLine1.style.transform = `translateY(-75vh) rotate(${Math.random()*90-45}deg)`;
-	logoLine2.style.transform = `translateX(-75vw) rotate(${Math.random()*90-45}deg)`;
-	logoLine3.style.transform = `translateY(75vh) rotate(${Math.random()*90-45}deg)`;
-	setTimeout(() => {
-		logoLine1.style.transition = "transform 2s, background-color .5s, color .5s, fill .5s";
-		logoLine2.style.transition = "transform 2s, background-color .5s, color .5s, fill .5s";
-		logoLine3.style.transition = "transform 2s, background-color .5s, color .5s, fill .5s";
-		logoIn();
-	}, 50);
-	setTimeout(() => {
-		navIn();
-		logoAnimation();
-		colorCycle();
-	}, 2000);
-}, 50);
-
+// Menus
 let currentMenu = "";
 function menuIn(menuName) {
 	colorCycleToggle = false;
-	logoOut();
 	navOut();
 	currentMenu = "#"+menuName;
 	let menuTarget = document.querySelector(currentMenu);
@@ -215,10 +223,17 @@ function menuOut() {
 	navIn();
 }
 
+
+
+// —————————————————————————————————————————————————————————————————————
+// FONTS
+// —————————————————————————————————————————————————————————————————————
+
 // Select font from menu
 function pickFont(selectedFont) {
 	document.querySelector(':root').style.setProperty("--activefont", `${selectedFont}`);
 	getAxisInfo(selectedFont, `fonts/${selectedFont}.ttf`);
+	playPercussion('C2');
 	menuOut();
 }
 
@@ -286,4 +301,158 @@ function getAxisInfo(fontFamily, fontUrl) {
 		}
 	}
 }
-  
+
+
+
+// —————————————————————————————————————————————————————————————————————
+// AUDIO
+// —————————————————————————————————————————————————————————————————————
+
+// Start audio context
+async function startAudioContext() {
+	await Tone.start();
+	console.log('Audio context is ready!');
+	document.querySelector('body').style.cursor = "unset";
+	document.querySelector('.intro-text').style.transform = "translate(-50%, 200px) rotate(-20deg)";
+	navIn();
+	settingsChangeColor();
+	logoOut();
+	document.querySelector('body').removeEventListener('click', startAudioContext);
+}
+setTimeout(() => {
+	document.querySelector('body').style.cursor = "pointer";
+	document.querySelector('.intro-text').style.transform = "translate(-50%, 0px) rotate(0)";
+	document.querySelector('body').addEventListener('click', startAudioContext);
+}, 2000)
+
+// Menu synth
+let synth = new Tone.PolySynth();
+synth.set({
+	oscillator: {
+		type: "sawtooth12"
+	},
+	envelope: {
+		attack: 0.05,
+		decay: 0.2,
+		sustain: .5,
+		release: .5
+	},
+	volume: -6
+}).toDestination();
+function playNote(freq) {
+	synth.triggerAttackRelease(freq, .15);
+}
+function changeSynth(synthType) {
+	synth.set({oscillator: {type: synthType}});
+}
+
+// Percussion sampler
+const percussionSampler = new Tone.Sampler({
+	urls: {
+		C0: "agogo-low.mp3",
+		D0: "agogo-high.mp3",
+		E0: "block.mp3",
+		F0: "bongo-low.mp3",
+		G0: "bongo-high.mp3",
+		A0: "cabasa-low.mp3",
+		B0: "cabasa-high.mp3",
+		C1: "castanet.mp3",
+		D1: "chimes.mp3",
+		E1: "clap.mp3",
+		F1: "conga-low.mp3",
+		G1: "conga-high.mp3",
+		A1: "cowbell.mp3",
+		B1: "crash-low.mp3",
+		C2: "crash-mid.mp3",
+		D2: "crash-high.mp3",
+		E2: "cuica-low.mp3",
+		F2: "cuica-high.mp3",
+		G2: "drum-low.mp3",
+		A2: "drum-high.mp3",
+		B2: "gong.mp3",
+		C3: "guiro.mp3",
+		D3: "guiro-hit.mp3",
+		E3: "hat-closed.mp3",
+		F3: "hat-open.mp3",
+		G3: "jingle.mp3",
+		A3: "kick-low.mp3",
+		B3: "kick-high.mp3",
+		C4: "kick-808.mp3",
+		D4: "ride-low.mp3",
+		E4: "ride-mid.mp3",
+		F4: "ride-high.mp3",
+		G4: "rim-low.mp3",
+		A4: "rim-high.mp3",
+		B4: "rim-ring.mp3",
+		C5: "shaker.mp3",
+		D5: "snare.mp3",
+		E5: "snare-deep.mp3",
+		F5: "snare-loose.mp3",
+		G5: "snare-808.mp3",
+		A5: "spin-down.mp3",
+		B5: "spin-up.mp3",
+		C6: "sticks.mp3",
+		D6: "tambourine.mp3",
+		E6: "tom0.mp3",
+		F6: "tom1.mp3",
+		G6: "tom2.mp3",
+		A6: "tom3.mp3",
+		B6: "tom4.mp3",
+		C7: "tom5.mp3",
+		D7: "triangle-muted.mp3",
+		E7: "triangle-open.mp3",
+		F7: "vibraslap.mp3",
+		G7: "whistle-low.mp3",
+		A7: "whistle-high.mp3",
+		B7: "woodblock-low.mp3",
+		C8: "woodblock-high.mp3",
+		D8: "woodblock-higher.mp3"
+	},
+	baseUrl: "sounds/percussion/",
+	volume: -10,
+}).toDestination();
+let percussionNotes = ["C0","D0","E0","F0","G0","A0","B0","C1","D1","E1","F1","G1","A1","B1","C2","D2","E2","F2","G2","A2","B2","C3","D3","E3","F3","G3","A3","B3","C4","D4","E4","F4","G4","A4","B4","C5","D5","E5","F5","G5","A5","B5","C6","D6","E6","F6","G6","A6","B6","C7","D7","E7","F7","G7","A7","B7","C8","D8"]
+function playPercussion(sample) {
+	if (sample == "random") {
+		percussionSampler.triggerAttackRelease(percussionNotes[Math.floor(Math.random()*percussionNotes.length)], 1);
+	} else {
+		percussionSampler.triggerAttackRelease(sample, 1);
+	}
+}
+
+const tomSampler = new Tone.Sampler({
+	urls: {
+		C3: "tom2.mp3",
+	},
+	baseUrl: "sounds/percussion/",
+	volume: -10,
+}).toDestination();
+function playTom(freq) {
+	tomSampler.triggerAttackRelease(freq, 1);
+}
+
+// Voice sampler ("Animalese")
+let voiceSamplerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"];
+let voiceSamplers = {}
+for (letter of voiceSamplerLetters) {
+	voiceSamplers[letter] = new Tone.Sampler({
+		urls: {
+			C3: `${letter}.mp3`
+		},
+		baseUrl: "sounds/voice/",
+		volume: -10,
+	}).toDestination();
+}
+function playVoice(phrase, pitch, speed) {
+	phrase = phrase.toLowerCase();
+	if (phrase != "") {
+		let letter = phrase[0];
+		if (letter != " ") {
+			voiceSamplers[letter].triggerAttackRelease(pitch[Math.floor(Math.random()*pitch.length)], .3);
+		}
+		let newPhrase = phrase.slice(1);
+		setTimeout(() => {
+			playVoice(newPhrase, pitch, speed);
+		}, speed)
+	}
+}
