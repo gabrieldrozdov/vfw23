5b1e58222c995ce58a70bb056ae4a8f9d65bb999
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2023-01-29 18:38:11 -0500

    first instrument finished!

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function logoIn() {
 }
 function logoOut() {
 	let logo = document.querySelector(".logo");
-	logo.style.transform = "translateY(-100vh) rotate(10deg)";
+	logo.style.transform = "translateY(-120vh) rotate(10deg)";
 	logoAnimationLoop = false;
 }
 let logoAnimationLoop = true;
@@ -153,6 +153,7 @@ function settingsOut() {
 		navIn();
 	}, 250);
 }
+
 // Settings functions
 function settingsFontSize(value) {
 	let currentValue = getComputedStyle(document.documentElement).getPropertyValue('--player-fontsize');
@@ -189,24 +190,26 @@ function settingsChangeColor() {
 	body.style.backgroundImage = `url("graphics/background-${color}.gif")`;
 	body.style.backgroundSize = `${Math.random()*50+50}px ${Math.random()*50+50}px`;
 }
+
 // Menus
 let currentMenu = "";
 function menuIn(menuName) {
 	colorCycleToggle = false;
+	instrumentOut();
 	navOut();
 	currentMenu = "#"+menuName;
 	let menuTarget = document.querySelector(currentMenu);
 	menuTarget.style.transform = "translateX(0) rotate(0deg)";
-	if (currentMenu == '#fontbox') {
-		let fonts = menuTarget.querySelectorAll('li');
-		for (let font of fonts) {
-			font.style.transition = "0s";
-			font.style.transform = `translate(${Math.random()*-500}px, ${Math.random()*-1000+500}px) rotate(${Math.random()*20-10}deg)`;
+	if (currentMenu == '#fontbox' || currentMenu == '#instrumentbox') {
+		let grid = menuTarget.querySelectorAll('li');
+		for (let item of grid) {
+			item.style.transition = "0s";
+			item.style.transform = `translate(${Math.random()*-500}px, ${Math.random()*-1000+500}px) rotate(${Math.random()*20-10}deg)`;
 		}
 		setTimeout(() => {
-			for (let font of fonts) {
-				font.style.transition = "background-color .2s, color .2s, fill .2s, transform 1s";
-				font.style.transform = `translate(0, 0) rotate(${Math.random()*20-10}deg)`;
+			for (let item of grid) {
+				item.style.transition = "background-color .2s, color .2s, fill .2s, transform 1s";
+				item.style.transform = `translate(0, 0) rotate(${Math.random()*20-10}deg)`;
 			}
 		}, 50)
 	}
@@ -220,6 +223,7 @@ function menuOut() {
 			font.style.transform = `translate(${Math.random()*-500}px, ${Math.random()*-200+100}px) rotate(${Math.random()*20-10}deg)`;
 		}
 	}
+	instrumentIn();
 	navIn();
 }
 
@@ -228,6 +232,17 @@ function menuOut() {
 // —————————————————————————————————————————————————————————————————————
 // FONTS
 // —————————————————————————————————————————————————————————————————————
+
+let fontOptions = [
+	'jost',
+	'bashful',
+	'powerpack',
+	'thatthenthis',
+	'minimochi',
+	'dreidel',
+	'galapagos',
+	'authenticremixed'
+]
 
 // Select font from menu
 function pickFont(selectedFont) {
@@ -238,14 +253,14 @@ function pickFont(selectedFont) {
 }
 
 // Read user-inputted font
-document.getElementById('fontbox-menu-item-custom').addEventListener('click', openDialog);
+document.getElementById('menubox-customfont').addEventListener('click', openDialog);
 function openDialog() {
 	// Make styled button open correct dialogue
-	document.getElementById('fontbox-menu-item-custom-input').click();
+	document.getElementById('menubox-customfont-input').click();
 }
 var newStyle = document.createElement('style');
 if (window.FileList && window.File && window.FileReader) {
-	document.getElementById('fontbox-menu-item-custom-input').addEventListener('change', event => {
+	document.getElementById('menubox-customfont-input').addEventListener('change', event => {
 		newStyle.remove();
 
 		// Validate file type
@@ -253,7 +268,7 @@ if (window.FileList && window.File && window.FileReader) {
 		let fileType = file.name.split('.').pop().toLowerCase();
 		if (fileType != 'ttf' && fileType != 'otf' && fileType != 'woff' && fileType != 'woff2') {
 			window.alert('That’s not a valid font file! Try a .ttf, .otf, .woff, or .woff2 file.');
-			document.getElementById('fontbox-menu-item-custom-input').value = null;
+			document.getElementById('menubox-customfont-input').value = null;
 			return;
 		}
 		let userFont = URL.createObjectURL(file);
@@ -265,7 +280,7 @@ if (window.FileList && window.File && window.FileReader) {
 		document.querySelector(':root').style.setProperty("--activefont", `user`);
 
 		// Reset input in case same file is selected
-		document.getElementById('fontbox-menu-item-custom-input').value = null;
+		document.getElementById('menubox-customfont-input').value = null;
 
 		// Transition out
 		getAxisInfo("user", userFont);
@@ -296,13 +311,306 @@ function getAxisInfo(fontFamily, fontUrl) {
 				axesInfo.push({name:axisName, min:axisMin, max:axisMax, default:axisDefault});
 			})
 			console.log(axesInfo);
+
+			// Set CSS variables
+			let temp = "";
+			for (let i=0; i<axesInfo.length && i<4; i++) {
+				document.querySelector(':root').style.setProperty(`--axis${i}-name`, `"${axesInfo[i].name}"`);
+				document.querySelector(':root').style.setProperty(`--axis${i}-min`, `${axesInfo[i].min}`);
+				document.querySelector(':root').style.setProperty(`--axis${i}-max`, `${axesInfo[i].max}`);
+				if (i>0) {
+					temp += ", ";
+				}
+				temp += `var(--axis${i}-name) var(--axis${i}-calculated)`;
+			}
+			document.querySelector(':root').style.setProperty(`--player-variation`, `${temp}`);
+			initializeInstrumentAxes();
+			console.log(getComputedStyle(document.body).getPropertyValue('--player-variation'));
 		} else {
 			console.log("No variable axes!");
+			axesInfo = [];
+			initializeInstrumentAxes();
 		}
 	}
 }
 
 
+
+// —————————————————————————————————————————————————————————————————————
+// INSTRUMENTS
+// —————————————————————————————————————————————————————————————————————
+
+let instrumentOptions = ['sequencer'];
+let activeInstrument = "";
+let previousInstrument = "";
+let instrumentPlay = false;
+let variableMultiplier = [0, 0, 0, 0];
+
+function instrumentIn() {
+	let instrumentParent = document.querySelector(".instruments");
+	if (previousInstrument != activeInstrument) {
+		let previousInstrumentDOM = document.querySelector("#"+previousInstrument);
+		previousInstrumentDOM.style.display = "none";
+		previousInstrument = activeInstrument;
+	}
+	let instrument = document.querySelector("#"+activeInstrument);
+	instrument.style.display = "block";
+	instrumentParent.style.transform = `translateY(0) rotate(0)`;
+}
+function instrumentOut() {
+	let instrumentParent = document.querySelector(".instruments");
+	instrumentParent.style.transform = "translateY(100vh) rotate(10deg)";
+	instrumentPlay = false;
+}
+
+// Select instrument from menu
+function pickInstrument(selectedInstrument) {
+	playPercussion('C2');
+	activeInstrument = selectedInstrument;
+	instrumentIn();
+	initializeInstrumentAxes();
+	menuOut();
+}
+
+// Initalize instrument to work with font axes
+let sequencerTextOptions = [
+	"What are you listening to",
+	"I like trains",
+	"ABCDEFGHIJ 0123456789",
+	"Hi mom",
+	"How is the economy today",
+	"Lavender lattes are superior",
+	"Beep boop bop",
+	"Ahhhhhhh Im so scared",
+	"OoOoOoOo EeEeEeEe",
+	"Tonight Im eating a smoothie",
+	"Will you dance with me",
+	"Heres a good joke",
+	"Meep moop",
+	"Oops I got engaged",
+	"Hello father",
+	"Check out my moves",
+	"Sorry I was busy playing Animal Crossing",
+	"Tomorrow Im too busy",
+	"Will you engage with me",
+	"Fourty two",
+	"Next project will be less annoying",
+	"Moonbug moonie chimkin chimmie mayor dinkins LIMMY",
+	"What are we having for dinner",
+	"Next question please",
+	"Im having tears for dinner"
+]
+function initializeInstrumentAxes() {
+	instrumentPlay = true;
+	if (activeInstrument == 'sequencer') {
+		// Make sure current speed button is active
+		let activeSpeed = document.querySelector(`[data-sequencer-speed='${sequencerSpeed}']`);
+		activeSpeed.dataset.sequencerSpeedActive = '1';
+
+		// Initalize all axes to not show
+		let sequencerAxes = document.querySelectorAll(".instrument-sequencer-beats-axis");
+		sequencerAxes[0].dataset.sequencerAxisActive = '0';
+		sequencerAxes[1].dataset.sequencerAxisActive = '0';
+		sequencerAxes[2].dataset.sequencerAxisActive = '0';
+		sequencerAxes[3].dataset.sequencerAxisActive = '0';
+
+		// Randomize display text
+		let displayText = document.querySelector(`.instrument-sequencer-display-text`);
+		displayText.innerText = sequencerTextOptions[Math.floor(Math.random()*sequencerTextOptions.length)];
+
+		// Check if font is actually variable
+		if (axesInfo.length == 0) {
+			document.querySelector(".instrument-sequencer-triggers").style.display = "none";
+			document.querySelector(".instrument-sequencer-beats").style.display = "none";
+			document.querySelector(".instrument-sequencer-speed").style.display = "none";
+			document.querySelector(".instrument-sequencer-beats-error").style.display = "flex";
+		} else {
+			document.querySelector(".instrument-sequencer-triggers").style.display = "grid";
+			document.querySelector(".instrument-sequencer-beats").style.display = "grid";
+			document.querySelector(".instrument-sequencer-speed").style.display = "grid";
+			document.querySelector(".instrument-sequencer-beats-error").style.display = "none";
+			for (let i=0; i<axesInfo.length && i<4; i++) {
+				sequencerAxes[i].dataset.sequencerAxisActive = '1';
+			}
+			sequencerLoop();
+		}
+	}
+	if (activeInstrument == 'talkbox') {
+		// TO DO ——————————————————————————————————————————————————————————————————————
+	}
+	if (activeInstrument == 'texturizer') {
+		// TO DO ——————————————————————————————————————————————————————————————————————
+	}
+	if (activeInstrument == 'oscillator') {
+		// TO DO ——————————————————————————————————————————————————————————————————————
+	}
+}
+
+// Sequencer
+let sequencerSpeed = 400;
+let sequencerBeat = 0;
+let sequencerSoundsPercussion = [
+	['A3','D5','E3','E1'],
+	['C3','A1','E0','E4'],
+	['D6','F7','D7','F2'],
+	['C0','G0','B7','B5']
+];
+let sequencerSoundsToms = [
+	['100', '120', '140', '160'],
+	['180', '200', '220', '240'],
+	['260', '280', '300', '320'],
+	['340','360','380','400']
+];
+let sequencerSoundsNote = [
+	['C2','E2','G2','B2'],
+	['C1','E1','G1','B1'],
+	['C3','E3','G3','B3'],
+	['C4','E4','G4','B4']
+];
+let sequencerSoundsVoice = [
+	['a','b','c','d'],
+	['1','2','3','4'],
+	['e','f','g','h'],
+	['5','6','7','8']
+];
+let sequencerSoundsVoicePitch = [
+	['C3','D3',"E3"],
+	['C4','D4',"E4"],
+	['C2','D2','E2'],
+	['C1','D1','E1']
+]
+function sequencerLoop() {
+	setTimeout(() => {
+		if (instrumentPlay == false) {
+			sequencerBeat = 0;
+			return
+		}
+		if (sequencerBeat >= 8) {
+			sequencerBeat = 0;
+		}
+		document.querySelector(".instrument-sequencer-beats-error").style.display = "none";
+		for (let i=0; i<4 && i<axesInfo.length; i++) {
+			let sequencerActive = document.querySelector(`[data-sequencer-axis="${i}"] [data-sequencer-beat="${sequencerBeat}"]`);
+			let currentValue = sequencerActive.dataset.sequencerBeatValue;
+			if (currentValue == 0) {
+				document.querySelector(':root').style.setProperty(`--axis${i}-percent`, `${0}`);
+			}
+			if (currentValue == 1) {
+				document.querySelector(':root').style.setProperty(`--axis${i}-percent`, `${.25}`);
+				if (sequencerMode == 0) {
+					playPercussion(sequencerSoundsPercussion[i][0]);
+				} else if (sequencerMode == 1) {
+					playTom(sequencerSoundsToms[i][0]);
+				} else if (sequencerMode == 2) {
+					playPiano(sequencerSoundsNote[i][0]);
+				} else if (sequencerMode == 3) {
+					playVoice(sequencerSoundsVoice[i][0], sequencerSoundsVoicePitch[i], 100);
+				}
+			} else if (currentValue == 2) {
+				document.querySelector(':root').style.setProperty(`--axis${i}-percent`, `${.5}`);
+				if (sequencerMode == 0) {
+					playPercussion(sequencerSoundsPercussion[i][1]);
+				} else if (sequencerMode == 1) {
+					playTom(sequencerSoundsToms[i][1]);
+				} else if (sequencerMode == 2) {
+					playPiano(sequencerSoundsNote[i][1]);
+				} else if (sequencerMode == 3) {
+					playVoice(sequencerSoundsVoice[i][1], sequencerSoundsVoicePitch[i], 100);
+				}
+			} else if (currentValue == 3) {
+				document.querySelector(':root').style.setProperty(`--axis${i}-percent`, `${.75}`);
+				if (sequencerMode == 0) {
+					playPercussion(sequencerSoundsPercussion[i][2]);
+				} else if (sequencerMode == 1) {
+					playTom(sequencerSoundsToms[i][2]);
+				} else if (sequencerMode == 2) {
+					playPiano(sequencerSoundsNote[i][2]);
+				} else if (sequencerMode == 3) {
+					playVoice(sequencerSoundsVoice[i][2], sequencerSoundsVoicePitch[i], 100);
+				}
+			} else if (currentValue == 4) {
+				document.querySelector(':root').style.setProperty(`--axis${i}-percent`, `${1}`);
+				if (sequencerMode == 0) {
+					playPercussion(sequencerSoundsPercussion[i][3]);
+				} else if (sequencerMode == 1) {
+					playTom(sequencerSoundsToms[i][3]);
+				} else if (sequencerMode == 2) {
+					playPiano(sequencerSoundsNote[i][3]);
+				} else if (sequencerMode == 3) {
+					playVoice(sequencerSoundsVoice[i][3], sequencerSoundsVoicePitch[i], 100);
+				}
+			}
+			let sequencerBeats = document.querySelectorAll(`[data-sequencer-axis="${i}"] [data-sequencer-beat]`);
+			for (let beat of sequencerBeats) {
+				beat.style.backgroundColor = "var(--dark)";
+			}
+			sequencerActive.style.backgroundColor = "var(--light)";
+		}
+		sequencerBeat++;
+		sequencerLoop();
+	}, sequencerSpeed)
+}
+function sequencerRandomizeBeats() {
+	let iteration = 0;
+	let loop = setInterval(() => {
+		playBlock(Math.random()*150+150);
+		let sequencerBeats = document.querySelectorAll(`[data-sequencer-beat]`);
+		for (let beat of sequencerBeats) {
+			beat.dataset.sequencerBeatValue = Math.floor(Math.random()*5);
+		}
+		iteration++;
+		if (iteration >= 3) {
+			clearInterval(loop);
+		}
+	}, 100)
+}
+let sequencerMode = 0;
+function sequencerChangeMode() {
+	sequencerMode++;
+	if (sequencerMode >= 5) {
+		sequencerMode = 0;
+	}
+}
+function sequencerBeatAdjust(e) {
+	let currentValue = e.dataset.sequencerBeatValue;
+	if (currentValue >= 4) {
+		e.dataset.sequencerBeatValue = 0;
+	} else {
+		e.dataset.sequencerBeatValue++;
+	}
+	if (currentValue == 0) {
+		playBlock('F3');
+	} else if (currentValue == 1) {
+		playBlock('G3');
+	} else if (currentValue == 2) {
+		playBlock('A3');
+	} else if (currentValue == 3) {
+		playBlock('B3');
+	} else if (currentValue == 4) {
+		playBlock('C3');
+	}
+}
+function sequencerSpeedAdjust(e) {
+	if (e.dataset.sequencerSpeedActive == '1') {
+		e.dataset.sequencerSpeedActive = '0';
+		instrumentPlay = false;
+		for (let beat of document.querySelectorAll(`[data-sequencer-beat]`)) {
+			beat.style.backgroundColor = "var(--dark)";
+		}
+	} else if (instrumentPlay == true) {
+		sequencerSpeed = e.dataset.sequencerSpeed;
+		document.querySelector(':root').style.setProperty(`--player-variation-speed`, `${sequencerSpeed}ms`);
+		let prevActive = document.querySelector("[data-sequencer-speed-active='1']");
+		prevActive.dataset.sequencerSpeedActive = '0';
+		e.dataset.sequencerSpeedActive = '1';
+	} else {
+		sequencerSpeed = e.dataset.sequencerSpeed;
+		e.dataset.sequencerSpeedActive = '1';
+		document.querySelector(':root').style.setProperty(`--player-variation-speed`, `${sequencerSpeed}ms`);
+		instrumentPlay = true;
+		sequencerLoop();
+	}
+}
 
 // —————————————————————————————————————————————————————————————————————
 // AUDIO
@@ -313,11 +621,23 @@ async function startAudioContext() {
 	await Tone.start();
 	console.log('Audio context is ready!');
 	document.querySelector('body').style.cursor = "unset";
-	document.querySelector('.intro-text').style.transform = "translate(-50%, 200px) rotate(-20deg)";
-	navIn();
-	settingsChangeColor();
-	logoOut();
 	document.querySelector('body').removeEventListener('click', startAudioContext);
+	document.querySelector('.intro-text').style.transform = "translate(-50%, 200px) rotate(-20deg)";
+	colorCycleToggle = false;
+	logoOut();
+
+	// Pick random font
+	let randomFont = fontOptions[Math.floor(Math.random()*fontOptions.length)];
+	document.querySelector(':root').style.setProperty("--activefont", `${randomFont}`);
+	getAxisInfo(randomFont, `fonts/${randomFont}.ttf`);
+
+	// Pick random instrument
+	let randomInstrument = instrumentOptions[Math.floor(Math.random()*instrumentOptions.length)]
+	activeInstrument = previousInstrument = randomInstrument;
+	console.log(activeInstrument);
+	instrumentIn();
+
+	navIn();
 }
 setTimeout(() => {
 	document.querySelector('body').style.cursor = "pointer";
@@ -344,6 +664,29 @@ function playNote(freq) {
 }
 function changeSynth(synthType) {
 	synth.set({oscillator: {type: synthType}});
+}
+
+// Piano sampler
+const pianoSampler = new Tone.Sampler({
+	urls: {
+		C1: "c1.mp3",
+		C2: "c2.mp3",
+		C3: "c3.mp3",
+		C4: "c4.mp3",
+		C5: "c5.mp3",
+		C6: "c6.mp3"
+	},
+	envelope: {
+		attack: 0.05,
+		decay: 0.2,
+		sustain: .5,
+		release: .5
+	},
+	baseUrl: "sounds/piano/",
+	volume: -10,
+}).toDestination();
+function playPiano(sample) {
+	pianoSampler.triggerAttackRelease(sample, .5, "+0", `+${Math.random()*.5+.5}`);
 }
 
 // Percussion sampler
@@ -420,6 +763,7 @@ function playPercussion(sample) {
 	}
 }
 
+// Pitched tom sampler
 const tomSampler = new Tone.Sampler({
 	urls: {
 		C3: "tom2.mp3",
@@ -431,10 +775,22 @@ function playTom(freq) {
 	tomSampler.triggerAttackRelease(freq, 1);
 }
 
+// Pitched woodblock sampler
+const blockSampler = new Tone.Sampler({
+	urls: {
+		C4: "woodblock-higher.mp3",
+	},
+	baseUrl: "sounds/percussion/",
+	volume: -10,
+}).toDestination();
+function playBlock(freq) {
+	blockSampler.triggerAttackRelease(freq, 1);
+}
+
 // Voice sampler ("Animalese")
 let voiceSamplerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"];
 let voiceSamplers = {}
-for (letter of voiceSamplerLetters) {
+for (let letter of voiceSamplerLetters) {
 	voiceSamplers[letter] = new Tone.Sampler({
 		urls: {
 			C3: `${letter}.mp3`
@@ -448,7 +804,7 @@ function playVoice(phrase, pitch, speed) {
 	if (phrase != "") {
 		let letter = phrase[0];
 		if (letter != " ") {
-			voiceSamplers[letter].triggerAttackRelease(pitch[Math.floor(Math.random()*pitch.length)], .3);
+			voiceSamplers[letter].triggerAttackRelease(pitch[Math.floor(Math.random()*pitch.length)], .5);
 		}
 		let newPhrase = phrase.slice(1);
 		setTimeout(() => {
